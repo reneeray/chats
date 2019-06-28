c2431c32d8af7450eee00bc4502991e8e0ec964b
Stephanie Hatten <ReneeRay@Stephanies-MacBook-Pro.local>
2019-06-28 15:59:16 -0700

    created my first component in react

---
+++ src/TextInput.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class Textinput extends React.Component {
+
+    render (){
+        return( <div className="text-input">
+        <input />
+        <button>SEND</button>
+        
+    </div>)
+    
+    }
+}
+
+export default Textinput
