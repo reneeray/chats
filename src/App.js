e93f3c347d901f737f4dd91c98fe97cce34d7bcc
Stephanie Hatten <ReneeRay@Stephanies-MacBook-Pro.local>
2019-07-02 21:09:35 -0700

    added message array, made app.js a 'stateful' app, formatted message box

--- src/App.js
+++ src/App.js
@@ -4,17 +4,34 @@ import './App.css'
 import logo from './chats_logo.png';
 import TextInput from './TextInput'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="app-header">
-        <img className="logo" src={logo} alt="logo"/>
-        Chats
+class App extends React.Component {
+  state = {
+    messages: []
+  }
+  sendMessage = (m) => {
+    var { messages } = this.state
+    var messages = [...messages, m]
+    this.setState({ messages })
+  }
+  render() {
+    var { messages } = this.state
+    console.log(messages)
+    return (
+      <div className="App">
+        <header className="app-header">
+          <img className="logo" src={logo} alt="logo" />
+          Chats
       </header>
-      
-      <TextInput />
-    </div>
-  );
+        <main>
+          {messages.map((msg,i)=>{
+            return <div key={i}>
+              {msg}
+            </div>
+          })}
+        </main>
+        <TextInput sendMessage={this.sendMessage} />
+      </div>
+    );
+  }
 }
-
 export default App;
--- src/TextInput.js
+++ src/TextInput.js
@@ -1,18 +1,38 @@
 import React from 'react'
-import up_arrow from './up_arrow.png';
+/*import up_arrow from './up_arrow.png';*/
+import { FiSend } from "react-icons/fi"
 
 class Textinput extends React.Component {
+    state = {
+        text: '',
 
-    render (){
-        return( <div className="text-input">
-        <input />
+    }
+    send = () => {
+        this.props.sendMessage(this.state.text)
+        this.setState({ text: '' })
+    }
+    keyPress = (e) => {
+        if(e.key==="Enter" && this.state.text){
+            this.send()
+        }
+    }
+    render() {
+        return (<div className="text-input">
+            <input value={this.state.text}
+                placeholder="Type your message here..."
+                onKeyPress={this.keyPress}
+                onChange={e => this.setState({ text: e.target.value })}
+            />
 
-        <button>
-        <img className="up_arrow" src={up_arrow} alt="up_arrow"/>
-        </button>
-   
-    </div>)
-    
+            <button onClick={this.send}
+                disabled={!this.state.text}>
+                
+                    <FiSend style={{height:17, width:17}}/>
+            
+            </button>
+
+        </div>)
+
     }
 }
 
